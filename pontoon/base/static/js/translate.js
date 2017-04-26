9935fb1b8cc039a8da41235ca54a377b803b608d
Matjaž Horvat <matjaz.horvat@gmail.com>
2017-04-26 20:57:47 +0200

    Update translate.js

--- pontoon/base/static/js/translate.js
+++ pontoon/base/static/js/translate.js
@@ -25,6 +25,7 @@ var Pontoon = (function (my) {
     renderEntity: function (index, entity) {
       var self = this,
           status = self.getEntityStatus(entity),
+          openSans = (['Latin', 'Greek', 'Cyrillic', 'Vietnamese'].indexOf(self.locale.script) > -1) ? ' open-sans' : '',
           source_string = (entity.original_plural && self.locale.nplurals < 2) ? entity.marked_plural : entity.marked,
           li = $('<li class="entity' +
         (' ' + status) +
@@ -35,7 +36,7 @@ var Pontoon = (function (my) {
         '<span class="status fa' + (self.user.canTranslate() ? '' : ' unselectable') + '"></span>' +
         '<p class="string-wrapper">' +
           '<span class="source-string">' + source_string + '</span>' +
-          '<span class="translation-string" dir="auto" lang="' + self.locale.code + '">' +
+          '<span class="translation-string' + openSans + '" dir="' + self.locale.direction + '" lang="' + self.locale.code + '" data-script="' + self.locale.script + '">' +
             self.markPlaceables(entity.translation[0].string || '') +
           '</span>' +
         '</p>' +
@@ -132,7 +133,7 @@ var Pontoon = (function (my) {
             $.each(data, function() {
               list.append('<li class="suggestion" title="Copy Into Translation (Tab)">' +
                 '<header>' + this.locale__name + '<span class="stress">' + this.locale__code + '</span></header>' +
-                '<p class="translation" dir="auto" lang="' + this.locale__code + '">' +
+                '<p class="translation" dir="' + this.locale__direction + '" lang="' + this.locale__code + '" data-script="' + this.locale__script + '">' +
                   self.markPlaceables(this.string) +
                 '</p>' +
                 '<p class="translation-clipboard">' +
@@ -271,10 +272,10 @@ var Pontoon = (function (my) {
                       ((self.user.id && (self.user.id === this.uid) || self.user.canTranslate()) ? '<button class="delete fa" title="Delete"></button>' : '') +
                     '</menu>' +
                   '</header>' +
-                  '<p class="translation" dir="auto" lang="' + self.locale.code + '">' +
+                  '<p class="translation" dir="' + self.locale.direction + '" lang="' + self.locale.code + '" data-script="' + self.locale.script + '">' +
                     self.markPlaceables(this.translation) +
                   '</p>' +
-                  '<p class="translation-diff" dir="auto" lang="' + self.locale.code + '">' +
+                  '<p class="translation-diff" dir="' + self.locale.direction + '" lang="' + self.locale.code + '" data-script="' + self.locale.script + '">' +
                     ((i > 0) ? self.diff(data[0].translation, this.translation) : self.markPlaceables(this.translation)) +
                   '</p>' +
                   '<p class="translation-clipboard">' +
@@ -2271,22 +2272,24 @@ var Pontoon = (function (my) {
      * Update all translations in localStorage on server
      */
     syncLocalStorageOnServer: function() {
-      if (localStorage.length !== 0) {
-        var len = this.entities.length;
-        for (var i = 0; i < len; i++) {
-          var entity = this.entities[i],
-              key = this.getLocalStorageKey(entity),
-              value = localStorage[key];
-          if (value) {
-            value = JSON.parse(localStorage[key]);
-            this.updateOnServer(entity, value.translation, false);
-            delete localStorage[key];
-          }
-        }
-
-        // Clear all other translations
-        localStorage.clear();
+      if (! (localStorage instanceof Storage) || localStorage.length === 0) {
+        return;
       }
+
+      var len = this.entities.length;
+      for (var i = 0; i < len; i++) {
+        var entity = this.entities[i],
+            key = this.getLocalStorageKey(entity),
+            value = localStorage[key];
+        if (value) {
+          value = JSON.parse(localStorage[key]);
+          this.updateOnServer(entity, value.translation, false);
+          delete localStorage[key];
+        }
+      }
+
+      // Clear all other translations
+      localStorage.clear();
     },
 
 
@@ -2811,6 +2814,17 @@ var Pontoon = (function (my) {
 
 
     /*
+     * Update textarea lang and dir attributes
+     */
+    updateTextareaAttributes: function () {
+      $('#translation')
+        .attr('dir', this.locale.direction)
+        .attr('lang', this.locale.code)
+        .attr('data-script', this.locale.script);
+    },
+
+
+    /*
      * Update profile menu links and contents
      */
     updateProfileMenu: function () {
@@ -2903,6 +2917,7 @@ var Pontoon = (function (my) {
       self.updateMainMenu();
       self.updateProjectInfo();
       self.updateProfileMenu();
+      self.updateTextareaAttributes();
       self.updateSaveButtons();
       self.resetTimeRange();
       self.updateAuthors();
